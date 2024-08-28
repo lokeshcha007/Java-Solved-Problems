e74701965c48a8314769813738ad48f63926d214
Lokeshch420 <lokeshcha2002@gmail.com>
2024-08-28 17:04:11 +0530

    lowercase

--- Case.java
+++ Case.java
@@ -4,7 +4,15 @@ public class Case {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        System.out.print("Enter your character ");
+        System.out.print("Enter your character :  ");
+        String value = scn.nextLine();
+        if (value.charAt(0) >= 'a' && value.charAt(0) <= 'z') {
+            System.out.println("Given input is an lowercase alphabet " + value);
+        } else if (value.charAt(0) >= 'A' && value.charAt(0) <= 'Z') {
+            System.out.println("Given input is an Uppercase alphabet " + value);
+        } else {
+            System.out.println("Enter the valud Charcater ! ");
+        }
 
     }
 }
