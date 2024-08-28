9a3cc624659ff90412159ac9e96c12caba0837c7
Lokeshch420 <lokeshcha2002@gmail.com>
2024-08-28 16:55:34 +0530

    vowelor constanr

---
+++ Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Case {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter your character ");
+
+    }
+}
